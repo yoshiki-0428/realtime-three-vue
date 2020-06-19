9e2261b9473a0fe219b8c37ee25902bc729d0426
Yoshiki Ohashi <rrrtcdish2@gmail.com>
2020-06-19 10:03:15 +0900

    add ue yoko sita migi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,14 @@
   <div class="hello">
     <div id="stage">
     </div>
-    <button @click="interver(move)">↓</button>
-    <button @click="interver(moveRight)">→</button>
+    <button @click="move({ value: Math.min(...keyFlame.x) - 1, part: 'rotation[x]' }, keyFlame.x)">↑</button>
+    <button @click="move({ value: Math.max(...keyFlame.x) + 1, part: 'rotation[x]' }, keyFlame.x)">↓</button>
+    <button @click="move({ value: Math.max(...keyFlame.y) + 1, part: 'rotation[y]' }, keyFlame.y)">→</button>
+    <button @click="move({ value: Math.min(...keyFlame.y) - 1, part: 'rotation[y]' }, keyFlame.y)">←</button>
+    <div>
+      {{ this.keyFlame.x }}
+      {{ this.keyFlame.y }}
+    </div>
   </div>
 </template>
 
@@ -12,21 +18,15 @@ import * as THREE from 'three';
 
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  },
   data () {
     return {
       scene: this.scene,
       renderer: this.renderer,
       camera: this.camera,
       light: this.light,
-      geometry: this.geometry,
-      material: this.material,
       mesh: this.mesh,
       mixer: undefined,
       action: {},
-      lastTime: 0,
       keyFlame: {
         x: [0],
         y: [0]
@@ -34,29 +34,7 @@ export default {
     }
   },
   created() {
-    // === scene ===
-    this.scene = new THREE.Scene();
-    // === renderer ===
-    this.renderer = new THREE.WebGLRenderer({alpha: true});
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    // === camera ===
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight), 1, 1000);
-    this.camera.position.set(0, 0, 15);
-    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-    // === light ===
-    this.light = new THREE.DirectionalLight(0xffffff);
-    this.light.position.set(0, 0, 10);
-    // === model ===
-    const cubeSize = 2.5;
-    this.geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-    this.material = new THREE.MeshNormalMaterial();
-    this.mesh = new THREE.Mesh(new THREE.CubeGeometry(5, 5, 5), this.material);
-
-    // === sceneにmodel,light, cameraを追加 ===
-    this.scene.add(this.camera);
-    this.scene.add(this.light);
-    this.scene.add(this.mesh);
-    this.mixer = new THREE.AnimationMixer(this.scene);
+    this.setUp();
     this.animate();
   },
   mounted() {
@@ -64,6 +42,30 @@ export default {
     stage.appendChild(this.renderer.domElement)
   },
   methods: {
+    setUp () {
+      // === scene ===
+      this.scene = new THREE.Scene();
+      // === renderer ===
+      this.renderer = new THREE.WebGLRenderer({alpha: true});
+      this.renderer.setSize(window.innerWidth, window.innerHeight);
+      // === camera ===
+      this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight), 1, 1000);
+      this.camera.position.set(0, 0, 15);
+      this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+      // === light ===
+      const light = new THREE.DirectionalLight(0xffffff);
+      light.position.set(0, 0, 10);
+      // === model ===
+      const cubeSize = 2.5;
+      const material = new THREE.MeshNormalMaterial();
+      this.mesh = new THREE.Mesh(new THREE.CubeGeometry(cubeSize, cubeSize, cubeSize), material);
+
+      // === sceneにmodel,light, cameraを追加 ===
+      this.scene.add(this.camera);
+      this.scene.add(light);
+      this.scene.add(this.mesh);
+      this.mixer = new THREE.AnimationMixer(this.scene);
+    },
     animate () {
       if (this.mixer) {
         this.mixer.update(0.1);
@@ -71,13 +73,13 @@ export default {
       this.renderer.render(this.scene, this.camera);
       requestAnimationFrame(this.animate);
     },
-    move() {
+    move(logData, keyFlame) {
       const tracks = [];
-      this.keyFlame.x.push(Math.max(...this.keyFlame.x) + 1);
-      tracks.push({ name: `${this.mesh.uuid}.rotation[x]`,
+      keyFlame.push(logData.value);
+      tracks.push({ name: `${this.mesh.uuid}.${logData.part}`,
         type: 'vector',
-        times: [...Array(this.keyFlame.x.length).keys()],
-        values: this.keyFlame.x
+        times: [...Array(keyFlame.length).keys()],
+        values: keyFlame
       });
       const clip = new THREE.AnimationClip.parse({ name: "hoge", duration: 5000, tracks: tracks });
       this.action = this.mixer.clipAction(clip);
@@ -100,9 +102,6 @@ export default {
       console.log(this.mixer.time)
       this.action.setLoop(THREE.LoopOnce, 1);
       this.action.play();
-    },
-    interver(func) {
-      setInterval(func, 500);
     },
   },
 }
